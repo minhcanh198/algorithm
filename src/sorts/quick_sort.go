3ec81be62247910f878120a35c036b00e5a87303
Nguyen Minh Canh <silverben98@gmail.com>
2024-02-26 23:31:39 +0700

    quicksort pivot left

--- src/sorts/quick_sort.go
+++ src/sorts/quick_sort.go
@@ -1,5 +1,7 @@
 package sorts
 
+import "fmt"
+
 func QuickSort(arr []int, l int, r int) {
 	if l >= r {
 		return
@@ -18,5 +20,30 @@ func QuickSort(arr []int, l int, r int) {
 
 	swap(arr, newPivot, pivot)
 	QuickSort(arr, l, newPivot-1)
-	QuickSort(arr, newPivot + 1, r)
+	QuickSort(arr, newPivot+1, r)
+}
+
+func QuickSortPivotLeft(arr []int, l int, r int) {
+	fmt.Println("input", arr)
+	if l >= r {
+		return
+	}
+
+	pivot := l
+	runIdx := l + 1
+	newPivot := l
+	for runIdx <= r {
+		if arr[runIdx] < arr[pivot] {
+
+			newPivot++
+			swap(arr, newPivot, runIdx)
+		}
+		runIdx++
+	}
+
+
+	swap(arr, newPivot, pivot)
+	QuickSortPivotLeft(arr, l, newPivot-1)
+
+	QuickSortPivotLeft(arr, newPivot+1, r)
 }
