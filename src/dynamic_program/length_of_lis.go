a6036767c2b212e1526d6c99c380fd46cf57c996
Canh Lio <silverben98@gmail.com>
2025-05-25 11:35:42 +0700

    done long increasing subsequence

--- src/dynamic_program/length_of_lis.go
+++ src/dynamic_program/length_of_lis.go
@@ -2,23 +2,25 @@ package dynamic_program
 
 // https://leetcode.com/problems/longest-increasing-subsequence/description/?envType=study-plan-v2&envId=top-interview-150
 func LengthOfLIS(nums []int) int {
+	if len(nums) <= 1 {
+		return 1
+	}
+
 	dp := make([]int, len(nums))
-	dp[0] = 1
+	maxNum := 1
+
+	for i := range nums {
+		dp[i] = 1
+	}
+
 	for i := 1; i < len(nums); i++ {
 		for j := 0; j < i; j++ {
 			if nums[j] < nums[i] {
 				dp[i] = maxInt(dp[i], dp[j]+1)
+				maxNum = maxInt(maxNum, dp[i])
 			}
 		}
+	}
 
-	}
-	maxNum := 0
-	for i := range dp {
-		maxNum = maxInt(maxNum, dp[i])
-	}
 	return maxNum
 }
-
-//
-//func LengthOfLIS(nums []int) int {
-//}
